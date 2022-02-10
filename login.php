fdeb2105570490923151c60c64d0f71717b7cdd3
AMBIVERTM <farhanumuzx@gmail.com>
2022-02-10 14:27:00 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,16 +4,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if (($username)) {
-      echo "Username is $username";
-    }else if($password){
-      echo "Password is $password";  
+    if (username($username)) {
+      echo "Username is ".$username;
+    }else if(password($password){
+      echo "Password is ".$password;  
     }else {
       echo "You don't put data yet";
     }
       
   }
   */
+  // define variables and set to empty values
+$username = $password = "";
   if ($_SERVER["REQUEST_METHOD"] == "POST") { 
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -21,4 +23,5 @@ $password = $_POST['password'];
 echo "Your username is :".$username;
 echo "Your password is :".$password;
   }
+
 ?>
